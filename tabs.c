27af495a089275ae0e13539c8acaa7be32437c23
Simon Giesemann <kwacky1@github.com>
2018-12-13 15:48:07 +1000

    Update tabs.c

--- tabs.c
+++ tabs.c
@@ -5,3 +5,5 @@ No tabs
 123456789
 i wonder if a line has lots of i's will it be shorter
 Than a sentance with at least the same amount of characters
+Are all spaces   created   equal
+one two  three   four    .
